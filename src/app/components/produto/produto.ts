f87deb952293bd39f861b2635015847895edac50
Alice Moraes <alicegamer35@gmail.com>
2026-07-14 17:15:40 -0300

    feat: adiciona pipe de formatação para nome e preço do produto

--- src/app/components/produto/produto.ts
+++ src/app/components/produto/produto.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output } from '@angular/core';
 import { UpperCasePipe, CurrencyPipe } from '@angular/common';
 import { PrecoFormatadoPipe } from '../../pipes/preco-formatado-pipe';
 
@@ -9,13 +9,7 @@ import { PrecoFormatadoPipe } from '../../pipes/preco-formatado-pipe';
   styleUrl: './produto.css',
 })
 export class Produto {
-  //produto = 'Notebook';
-  //preco = 2500;
-  //mostrarPreco = true;
-  //mostrarProduto = true;
-  produtos = [
-    {produto:'Monitor', preco: 1000},
-    {produto: 'Fonte Corsair', preco: 500},
-    {produto: 'Teclado', preco:300} 
-  ]
+ @Input() nome:string = '';
+ @Input() preco:number = 0;
+ 
 }
