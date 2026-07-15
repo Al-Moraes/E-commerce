62a698c1011e9a440de490a1f5038c653f06b158
Alice Moraes <alicegamer35@gmail.com>
2026-07-15 17:15:43 -0300

    refactor: atualiza de local componentes de produto e pipe

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,7 @@
 import { Component, signal } from '@angular/core';
 //import { RouterOutlet } from '@angular/router'; //Remove importação do RouterOutlet
 //import { Produto } from './components/produto/produto'; //Importa o componente Produto
-import { ListaProdutos } from './components/lista-produtos/lista-produtos';
+import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
 @Component({
   selector: 'app-root',
   imports: [ListaProdutos],
