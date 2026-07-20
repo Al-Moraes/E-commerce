7ef361422cca537b01a33820ab505cfe1f7c3d9f
Alice Moraes <alicegamer35@gmail.com>
2026-07-20 13:36:54 -0300

    feat: adiciona itens ao carrinho e calcula quantidade e total

--- src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -4,20 +4,21 @@ import { Produto } from '../produto/produto';
 import { computed } from '@angular/core';
 import { PrecoFormatadoPipe } from '../../../shared/pipes/preco-formatado-pipe';
 import { effect } from '@angular/core';
+import { UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-lista-produtos',
-  imports: [Produto, PrecoFormatadoPipe],
+  imports: [Produto, PrecoFormatadoPipe, UpperCasePipe],
   templateUrl: './lista-produtos.html',
   styleUrl: './lista-produtos.css',
 })
 export class ListaProdutos {
   produtos = signal([
-    {nome: 'Teclado Gamer', preco:49.99},
-    {nome: 'Mouse Gamer', preco:29.99},
-    {nome: 'Monitor Gamer', preco:599.99},
-    {nome: 'Desktop Gamer', preco:49.99},
-    {nome: 'Headset Gamer', preco:100.50}
+    {nome: 'Teclado Gamer', preco: 149.00},
+    {nome: 'Mouse Gamer', preco: 229.99},
+    {nome: 'Monitor Gamer', preco: 1599.99},
+    {nome: 'Desktop Gamer', preco: 4999.99},
+    {nome: 'Headset Gamer', preco: 699.99}
   ]);
   exibirProduto (nome: string){
     console.log ('Produto Selecionado: ', nome);
@@ -25,7 +26,7 @@ export class ListaProdutos {
   }
   adicionarProduto(){
     this.produtos.update(listaAtual => [
-      ...listaAtual, {nome:'Sony Playstation 5', preco: 10000}]);
+      ...listaAtual, {nome:'Processador Intel Core i5 14550FS', preco: 2500}]);
   }
   totalProdutos = computed (() => this.produtos().length);
   valorTotal = computed (() => { return this.produtos().reduce
@@ -33,8 +34,11 @@ export class ListaProdutos {
     
     substituirProdutos (){
       this.produtos.set([
-        {nome: 'Arroz Fazenda', preco: 400},
-        {nome: 'Feijão Timbiras', preco: 100}
+        {nome: 'Teclado', preco: 40},
+        {nome: 'Mouse', preco: 10},
+        {nome: 'Monitor', preco: 100},
+        {nome: 'Desktop', preco: 500},
+        {nome: 'Headset', preco: 25}
       ]);
     }
     constructor(){
@@ -51,4 +55,14 @@ export class ListaProdutos {
       });
     }
     produtoSelecionado = signal<string | null> (null);
+    carrinho = signal <{nome: string; preco: number }[]>([]);
+    adicionarAocarrinho(produto:{nome:string; preco: number}){
+      this.carrinho.update(listaAtual =>
+        [...listaAtual, produto]);}
+        
+    quantidadeCarrinho = computed(() => this.carrinho().length);
+    totalCarrinho = computed(() => {
+      return this.carrinho().reduce((total, item)=>
+        total + item.preco,0);
+    });
 }
