a3292b2779ba1668ca46350b69ff6a1bbfe84924
Alice Moraes <alicegamer35@gmail.com>
2026-07-24 16:57:37 -0300

    feat: adiciona httpInterceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,20 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { withInterceptors } from '@angular/common/http';
+import { httpInterceptor }  from './core/interceptors/http.interceptor';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes), 
     provideClientHydration(),
-    provideHttpClient(withFetch()),
-    
+    provideHttpClient(withFetch(),
+  withInterceptors([httpInterceptor])),
   ]
 };
