0f74ade004c7f514e77befd2932da2459f6911e4
Alice Moraes <alicegamer35@gmail.com>
2026-08-04 17:13:46 -0300

    feat: adiciona routerlink, header e MatButtonModule

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,18 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router'; //Remove importação do RouterOutlet
+import { RouterOutlet } from '@angular/router'; //Remove importação do RouterOutlet
 import { usuarioLogado, login, logout } from './core/auth';
+import { Header } from './shared/layout/header/header';
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, Header, MatButtonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('e-commerce');
-  nomeLoja = 'Lojas Estadunidenses'; 
   usuarioLogado = usuarioLogado;
   login = login;
   logout = logout;
