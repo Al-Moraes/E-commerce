f156fc53c085daa7f62c7f24e8ddcf8f84fb8104
Alice Moraes <alicegamer35@gmail.com>
2026-08-10 16:01:54 -0300

    feat: adiciona carrinho vazio

--- src/app/core/services/carrinho.service.ts
+++ src/app/core/services/carrinho.service.ts
@@ -2,6 +2,12 @@ import { Injectable } from "@angular/core";
 import { signal } from "@angular/core";
 import { computed } from "@angular/core";
 
+
+type ItemCarrinho = {
+    nome: string;
+    preco: number;
+}
+
 @Injectable({
     providedIn: 'root'
 })
@@ -9,16 +15,20 @@ import { computed } from "@angular/core";
 export class CarrinhoService{
 
 //! Estado Global
-private carrinho = signal<{nome: string; preco: number}[]>([]);
+private carrinho = signal<ItemCarrinho[]>([]);
 
 //? Seletores
 itens = computed(() => this.carrinho());
+
 quantidadeItens = computed(() => this.carrinho().length); //!Quantidade de itens do carrinho
+
 totalItens = computed(() =>
     this.carrinho().reduce((total, item) => total + item.preco, 0));
 
+carrinhoVazio = computed(() => this.carrinho().length === 0);
+
 // TODO: Ações
-adicionar(produto: {nome: string; preco: number}){
+adicionar(produto:ItemCarrinho){
     this.carrinho.update(lista => [...lista, produto]);
 }
 
