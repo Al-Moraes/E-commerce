f827b6786f9c14c72d749eea30b6b25b70a1372a
Alice Moraes <alicegamer35@gmail.com>
2026-08-14 14:03:08 -0300

    feat: adiciona mensagem de autenticação e função de saída

--- src/app/features/admin/admin/admin.ts
+++ src/app/features/admin/admin/admin.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { computed } from '@angular/core';
+import { inject } from '@angular/core';
+import { signal } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-admin',
@@ -6,4 +11,30 @@ import { Component } from '@angular/core';
   templateUrl: './admin.html',
   styleUrl: './admin.css',
 })
-export class Admin {}
+
+export class Admin {
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
+  totalProdutosCadastrados = signal(20);
+  pedidosPendentes = signal(3);
+  usuariosCadastrados = signal(8);
+
+  usuarioAtual = this.authService.usuarioAtual;
+
+  mensagemPerfil = computed(() => {
+    const usuario = this.usuarioAtual();
+
+    if (!usuario) {
+      return 'Nenhum usuário autenticado.';
+    }
+
+    return `Usuário autenticado como ${usuario.perfil}.`;
+  });
+
+  sair() {
+
+    this.authService.logout();
+    this.router.navigateByUrl('/login');
+  }
+}
