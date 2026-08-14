f539bf227cf30a98236357e5f6e5649a9ab14077
Alice Moraes <alicegamer35@gmail.com>
2026-08-14 16:07:43 -0300

    feat: adciona método de autenticação e rota ao acesso negado

--- src/app/features/acesso-negado/acesso-negado/acesso-negado.ts
+++ src/app/features/acesso-negado/acesso-negado/acesso-negado.ts
@@ -2,14 +2,22 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { AuthService } from '../../../core/services/auth.service';
 import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatAnchor } from "@angular/material/button";
 
 @Component({
   selector: 'app-acesso-negado',
-  imports: [RouterLink],
+  imports: [RouterLink, MatAnchor],
   templateUrl: './acesso-negado.html',
   styleUrl: './acesso-negado.css',
 })
 export class AcessoNegado {
   private authService = inject (AuthService);
-  sair = this.authService.logout();
+  private router = inject(Router);
+
+  sair() {
+    this.authService.logout();
+    this.router.navigateByUrl('/login');
+    return;
+  }
 }
