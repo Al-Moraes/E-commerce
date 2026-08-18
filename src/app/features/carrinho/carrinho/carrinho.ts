d05392b6da1071fd2187b7d145eed5648c67a989
Alice Moraes <alicegamer35@gmail.com>
2026-08-18 17:08:10 -0300

    styles: adiciona métodos e imports

--- src/app/features/carrinho/carrinho/carrinho.ts
+++ src/app/features/carrinho/carrinho/carrinho.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { CarrinhoFacade } from '../../../core/facades/carrinho.facade';
+import { Router } from '@angular/router';
+import { AuthFacade } from '../../../core/facades/auth.facade';
+import { PrecoFormatadoPipe } from '../../../shared/pipes/preco-formatado-pipe';
 
 @Component({
   selector: 'app-carrinho',
-  imports: [],
+  imports: [RouterLink, MatButtonModule, PrecoFormatadoPipe],
   templateUrl: './carrinho.html',
   styleUrl: './carrinho.css',
 })
-export class Carrinho {}
-//Lógica do carrinho virá em breve 
-//Por ora, apenas exibe a página de carrinho
+export class Carrinho {
+   carrinhoFacade = inject(CarrinhoFacade);
+  private router = inject(Router);
+  private authFacade = inject(AuthFacade);
+
+  removerItem (rmvItem: number) {
+    this.carrinhoFacade.limparCarrinho();
+  }
+
+  limparCarrinho() {
+    this.carrinhoFacade.limparCarrinho();
+  }
+
+  cancelarCompra() {
+    this.authFacade.sair();
+    this.carrinhoFacade.limparCarrinho();
+    this.router.navigateByUrl('/login');
+  }
+}
