ff2ba6c8d8a5a95ed33ec88bec4a2dcd220023e6
Alice Moraes <alicegamer35@gmail.com>
2026-08-18 13:27:22 -0300

    fear: cria diretório facades

---
+++ src/app/core/facades/auth.facade.ts
@@ -0,0 +1,37 @@
+import { Injectable } from "@angular/core";
+import { AuthService } from "../services/auth.service";
+import { inject } from "@angular/core";
+
+type Login = {
+    email:string;
+    senha:string;
+}
+
+@Injectable({providedIn: 'root'})
+
+export class AuthFacade {
+
+    private authService = inject(AuthService);
+
+    usuarioAtual = this.authService.usuarioAtual;
+    usuarioLogado = this.authService.usuarioLogado;
+    token = this.authService.token;
+    admin = this.authService.Admin;
+
+    realizarLogin(email:string, senha:string):boolean{
+        return this.authService.login(email,senha);
+    }
+
+    sair() {
+        this.authService.logout();
+    }
+
+    obterToken():string | null{
+        return this.authService.obterToken();
+    }
+
+    obterPerfil() {
+        return this.authService.obterPerfil();
+    }
+
+}
