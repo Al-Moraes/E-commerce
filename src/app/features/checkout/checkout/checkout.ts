d3b95da7481c27ecccbb6aecc21e0453e4acf56f
Alice Moraes <alicegamer35@gmail.com>
2026-08-18 17:11:14 -0300

    feat: adiciona imports e métodos de limpar carrinho

--- src/app/features/checkout/checkout/checkout.ts
+++ src/app/features/checkout/checkout/checkout.ts
@@ -9,16 +9,21 @@ import { ValidationErrors } from '@angular/forms';
 import { signal } from '@angular/core';
 import { AuthFacade } from '../../../core/facades/auth.facade';
 import { CarrinhoFacade } from '../../../core/facades/carrinho.facade';
+import { RouterLink } from "@angular/router";
+import { Router } from '@angular/router';
+import { PrecoFormatadoPipe } from '../../../shared/pipes/preco-formatado-pipe';
 
 @Component({
   selector: 'app-checkout',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, RouterLink, PrecoFormatadoPipe],
   templateUrl: './checkout.html',
   styleUrl: './checkout.css',
 })
 export class Checkout {
   
   carrinhoFacade = inject(CarrinhoFacade);
+  router = inject(Router);
+  authFacade = inject(AuthFacade);
 
   formulario = new FormGroup({
     nome: new FormControl('', [Validators.required, Validators.minLength(3), nomeSemNumeros]),
@@ -54,6 +59,10 @@ export class Checkout {
 
 compraFinalizada = signal (false)
 
+sair(){
+  this.authFacade.sair();
+  this.router.navigateByUrl('/login');
+}
 }
 
 function nomeSemNumeros(control: AbstractControl): ValidationErrors | null {
