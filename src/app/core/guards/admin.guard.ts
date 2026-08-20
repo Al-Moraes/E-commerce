6e2da0473a67c5ea7de9aa8a79badf98394453da
Alice Moraes <alicegamer35@gmail.com>
2026-08-20 13:34:42 -0300

    feat: cria diretório Guards

---
+++ src/app/core/guards/admin.guard.ts
@@ -0,0 +1,21 @@
+import { inject } from "@angular/core";
+import { Router } from "@angular/router";
+import { CanActivateFn } from "@angular/router";
+import { AuthFacade } from "../facades/auth.facade";
+
+export const adminGuard: CanActivateFn = () => {
+    const router = inject(Router);
+    const authFacade = inject(AuthFacade);
+
+    //! - 1) Verificar se o Usuário está logado.
+    if(!authFacade.usuarioLogado()) {
+        return router.createUrlTree(['/login']);
+    }
+    //! - 2) Verificar se o Usuário Atual(Logado), se ele tem perfil de administrador.
+    if(!authFacade.admin()) {
+        return router.createUrlTree(['/acesso-negado'])
+    }
+    //! - 3) Se o Usuário estiver logado e for administrador = ACESSO LIBERADOOOO
+
+    return true;
+};
